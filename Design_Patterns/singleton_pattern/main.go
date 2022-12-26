9ba11f0e06e9598be62a8cb84b256a45e51636f2
donggyu cho <cho4036@gmail.com>
2022-12-27 01:13:35 +0900

    singleton

--- Design_Patterns/singleton_pattern/main.go
+++ Design_Patterns/singleton_pattern/main.go
@@ -1,11 +1,15 @@
-package singleton_pattern
+package main
 
-import v1setting "golangStudy/Design_Patterns/singleton_pattern/setting"
+import (
+	"fmt"
+	v1setting "golangStudy/Design_Patterns/singleton_pattern/setting"
+)
 
 func main() {
 	setting := v1setting.GetInstance()
 	setting.SetColor("red")
+	fmt.Println(setting.Color())
 
 	setting2 := v1setting.GetInstance()
-	setting2.Color()
+	fmt.Println(setting2.Color())
 }
